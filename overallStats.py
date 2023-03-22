f6eb6563f098c89ac47d5af391d2c3142a0cbdb4
NorbertCode <54719382+NorbertCode@users.noreply.github.com>
2023-03-23 00:15:12 +0100

    SecondsToTime() now includes hours

---
+++ overallStats.py
@@ -0,0 +1,17 @@
+# Converts the amount of seconds to a string in a format like 1:15.2
+def SecondsToTime(seconds):
+    minutes = round(seconds // 60)
+    seconds -= minutes * 60
+
+    hours = round(minutes // 60)
+    minutes -= hours * 60
+
+
+    time = str(round(seconds, 2))
+    if (minutes > 0):
+        time = str(minutes) + ":" + time
+
+    if (hours > 0):
+        time = str(hours) + ":" + time
+
+    return time
