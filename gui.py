c7c3069f93a5959aea6e3944de84c0cbf320002d
NorbertCode <54719382+NorbertCode@users.noreply.github.com>
2023-03-25 19:08:16 +0100

    added a few labels

--- gui.py
+++ gui.py
@@ -7,13 +7,17 @@ class GUI:
         self.window.geometry('400x400')
         self.window.title('csTimer Analyst')
 
+        # --- Frames ---
         header = tk.Frame(self.window)
+        analysis = tk.Frame(self.window)
         stats = tk.Frame(self.window)
         graphs = tk.Frame(self.window)
         header.pack(side='top', padx=50)
+        analysis.pack(side='top', padx=50)
         stats.pack(side='left', padx=50)
         graphs.pack(side='bottom')
 
+        # --- Header ---
         logo = tk.Label(self.window, text='csTimer Analyst', font=('Bahnschrift', 28))
         logo.pack(in_=header, pady=10)
 
@@ -22,11 +26,18 @@ class GUI:
         self.fileLabel.pack(in_=header, side='left')
         self.browseButton.pack(in_=header, side='right')
 
-        analyseButton = tk.Button(self.window, text='Analyse', width=35)
-        analyseButton.pack(in_=header, pady=10)
+        analyseButton = tk.Button(self.window, text='Analyse', width=40)
+        analyseButton.pack(in_=analysis, pady=10)
 
+        # --- Stats ---
         self.pb = tk.Label(self.window, text='PB:')
         self.pb.pack(in_=stats)
+        self.timeSpent = tk.Label(self.window, text='Total time spent:')
+        self.timeSpent.pack(in_=stats)
+        self.daysInARow = tk.Label(self.window, text='Most days in a row:')
+        self.daysInARow.pack(in_=stats)
+        self.daysBreak = tk.Label(self.window, text='Most days break:')
+        self.daysBreak.pack(in_=stats)
 
         self.window.mainloop()
 
@@ -34,3 +45,5 @@ class GUI:
         filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("CSV files", "*.csv"),))
         if filename != '':
             self.fileLabel.configure(text=filename)
+
+GUI()
