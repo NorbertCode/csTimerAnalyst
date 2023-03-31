5387529fd73e94f0f5fb111660dbd8d76373adbc
NorbertCode <54719382+NorbertCode@users.noreply.github.com>
2023-03-31 18:50:05 +0200

    graphs added

--- gui.py
+++ gui.py
@@ -6,8 +6,6 @@ import overallStats as os
 
 class GUI:
     def __init__(self):
-
-
         self.window = tk.Tk()
         self.window.geometry('400x400')
         self.window.title('csTimer Analyst')
@@ -20,16 +18,16 @@ class GUI:
         header.pack(side='top', padx=50)
         stats.pack(side='top', pady=10)
         stats.columnconfigure(0, minsize=200)
-        graphs.pack(side='bottom')
+        graphs.pack(side='bottom', pady=50)
 
         # --- Header ---
         logo = tk.Label(header, text='csTimer Analyst', font=('Bahnschrift', 28))
         logo.pack(pady=10)
 
         self.fileLabel = tk.Label(header, text='Choose your session file')
-        self.browseButton = tk.Button(header, text='Browse', command=self.BrowseFiles)
         self.fileLabel.pack(side='left')
-        self.browseButton.pack(side='right')
+        browseButton = tk.Button(header, text='Browse', command=self.BrowseFiles)
+        browseButton.pack(side='right')
 
         # --- Stats ---
         meanText = tk.Label(stats, text='Total mean:')
@@ -57,16 +55,32 @@ class GUI:
         self.daysBreak = tk.Label(stats, text='0')
         self.daysBreak.grid(row=4, column=1, sticky='w')
 
-        self.mostSolves = tk.Label(stats, text='')
+        self.mostSolves = tk.Label(stats, text='', justify='left')
         self.mostSolves.grid(row=5, column=0, sticky='w')
+
+        # --- Graph buttons ---
+        # todo: implement arguments like in the cli version
+        self.timesGraph = tk.Button(graphs, text='Times graph', width=12)
+        self.timesGraph.grid(row=0, column=0, padx=5)
+        self.solvesByDateGraph = tk.Button(graphs, text='Solves by date', width=12)
+        self.solvesByDateGraph.grid(row=0, column=1, padx=5)
+        self.subchart = tk.Button(graphs, text='Sub chart', width=12)
+        self.subchart.grid(row=0, column=2, padx=5)
+        #! Since analyser is initialized after loading a file commands are set in ConfigureGraphButtons()
 
         self.window.mainloop()
 
     def BrowseFiles(self):
         fileName = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("CSV files", "*.csv"),))
         if fileName != '' and fileName.split('.')[-1] == 'csv':
-            self.fileLabel.configure(text=fileName.split('/')[-1])
+            self.fileLabel.configure(text=fileName.split('/')[-1]) #? will this work on linux?
             self.ShowValues(fileName)
+            self.ConfigureGraphButtons()
+
+    def ConfigureGraphButtons(self):
+        self.timesGraph.configure(command=self.analyser.ShowTimesGraph)
+        self.solvesByDateGraph.configure(command=self.analyser.ShowSolvesByDayGraph)
+        self.subchart.configure(command=self.analyser.ShowSubChart)
 
     def ShowValues(self, fileName):
         self.analyser = an.Analyser(fileName)
@@ -78,6 +92,7 @@ class GUI:
         self.daysBreak.configure(text=self.analyser.mostDaysBreak)
         mostSolvesDate = self.analyser.allDays[self.analyser.totalSolvesByDate.index(max(self.analyser.totalSolvesByDate))]
         timeSpentOnMostSolves = os.SecondsToTime(self.analyser.totalTimeByDate[self.analyser.totalSolvesByDate.index(max(self.analyser.totalSolvesByDate))])
-        self.mostSolves.configure(text='The most solves (' + str(max(self.analyser.totalSolvesByDate)) + ') were done on ' + str(mostSolvesDate) + '. The total time spent that day is ' + timeSpentOnMostSolves)
+        self.mostSolves.configure(text='The most solves (' + str(max(self.analyser.totalSolvesByDate)) + ') were done on ' + str(mostSolvesDate) + '.\n The total time spent that day is ' + timeSpentOnMostSolves)
 
-GUI()
+if __name__ == '__main__':
+    GUI()
